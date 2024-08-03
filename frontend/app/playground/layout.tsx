0255c7d4a12205a5b07fab9ebca2c286ed467495
R1shabh-Gupta <imailrishabhgupta@gmail.com>
2024-08-03 15:57:42 +0530

    feat: add new components, hooks, constants, types, and assets

---
+++ frontend/app/playground/layout.tsx
@@ -0,0 +1,24 @@
+import Header from "@/components/layout/header";
+import Sidebar from "@/components/layout/sidebar";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Next Shadcn Dashboard Starter",
+  description: "Basic dashboard with Next.js and Shadcn",
+};
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex">
+      <Sidebar />
+      <main className="w-full flex-1 overflow-hidden">
+        <Header />
+        {children}
+      </main>
+    </div>
+  );
+}
---
+++ frontend/components/layout/page-container.tsx
@@ -0,0 +1,22 @@
+import { ScrollArea } from "@/components/ui/scroll-area";
+import React from "react";
+
+export default function PageContainer({
+  children,
+  scrollable = false,
+}: {
+  children: React.ReactNode;
+  scrollable?: boolean;
+}) {
+  return (
+    <>
+      {scrollable ? (
+        <ScrollArea className="h-[calc(100dvh-52px)]">
+          <div className="h-full  p-4 md:px-8">{children}</div>
+        </ScrollArea>
+      ) : (
+        <div className="h-full  p-4 md:px-8">{children}</div>
+      )}
+    </>
+  );
+}
