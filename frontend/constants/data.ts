b0977dc867c8685ccde08d18dbcabc8eba4ac03f
R1shabh-Gupta <imailrishabhgupta@gmail.com>
2024-08-03 15:56:57 +0530

    chore(constants): add constants file for project configuration

---
+++ frontend/constants/data.ts
@@ -0,0 +1,28 @@
+import { NavItem } from "@/types";
+
+export const navItems: NavItem[] = [
+  {
+    title: "Playground",
+    href: "/playground",
+    icon: "dashboard",
+    label: "Playground",
+  },
+  {
+    title: "AutoPreprocessor",
+    href: "/playground/autopreprocessor",
+    icon: "autopreprocessor",
+    label: "AutoPreprocessor",
+  },
+  {
+    title: "Bird Eye",
+    href: "/playground/birdeye",
+    icon: "birdeye",
+    label: "Bird Eye",
+  },
+  {
+    title: "Login",
+    href: "/",
+    icon: "login",
+    label: "login",
+  },
+];
