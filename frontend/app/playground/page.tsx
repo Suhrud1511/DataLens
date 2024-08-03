295e582144a1d2a12d4bd4f896759429b4803867
R1shabh-Gupta <imailrishabhgupta@gmail.com>
2024-08-03 15:57:29 +0530

    feat(components): update Navbar and playground page components

--- frontend/app/playground/page.tsx
+++ frontend/app/playground/page.tsx
@@ -1,5 +1,15 @@
-const page = () => {
-  return <div>playground</div>;
-};
+import PageContainer from "@/components/layout/page-container";
 
-export default page;
+export default function page() {
+  return (
+    <PageContainer scrollable={true}>
+      <div className="space-y-2">
+        <div className="flex items-center justify-between space-y-2">
+          <h2 className="text-2xl font-bold tracking-tight">
+            Hi, Welcome back 👋
+          </h2>
+        </div>
+      </div>
+    </PageContainer>
+  );
+}
